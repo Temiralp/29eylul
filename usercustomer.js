e9fc2fe8252b292843ff7aa298f443d9b3e411dc
Temiralp <@gma>
2024-09-29 13:45:46 +0300

    gitignore eklendi

--- usercustomer.js
+++ usercustomer.js
@@ -1,3 +1,8 @@
 function customer(){
-    
+
 }
+
+git checkout -b test  komutu ile yeni bir dal açın
+bu dalda 2 commit yapın
+git checkout main komuut ile main dalına geçin
+git rebase test   komutu ile test dalını maine rebase edin
